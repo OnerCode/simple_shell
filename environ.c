ec18327264491507693c11d4ce6fa6950f330a7b
OnerCode <bsronipahm23@gmail.com>
2023-09-18 10:20:36 -0700

    Error on number of lines on function

---
+++ environ.c
@@ -0,0 +1,18 @@
+/**
+ * print_environment -  print env on output
+ *
+ * Return: void
+ */
+
+void print_environment(void)
+{
+	extern char **environ; /* acess the environment variables*/
+
+	char **env = environ;
+
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+}
